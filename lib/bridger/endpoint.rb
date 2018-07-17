d234cfe05e1bfbf8a8a27151bfa08069703de28c
Ismael Celis <ismaelct@gmail.com>
2018-07-17 15:37:59 +0100

    Endpoint

---
+++ lib/bridger/endpoint.rb
@@ -0,0 +1,68 @@
+module Bridger
+  class Endpoint
+    MUTATING_VERBS = [:post, :put, :patch, :delete].freeze
+
+    attr_reader :name, :verb, :path, :title, :scope, :action, :serializer
+
+    def initialize(name:, verb:, path:, title:, scope:, action:, serializer:, authorizer:)
+      @name = name
+      @verb = verb.to_sym
+      @path = path
+      @title = title
+      @scope = scope
+      @authorizer = authorizer
+      @action = action
+      @serializer = serializer
+    end
+
+    def builder
+      @builder ||= RelBuilder.new(
+        name,
+        verb,
+        path,
+        query_keys,
+        title
+      )
+    end
+
+    def run!(payload: {}, auth:, helper:)
+      auth.authorize! scope, authorizer, helper.params
+
+      presenter = action.run!(payload: payload, auth: auth)
+      raise "Missing serializer for #{name}" unless serializer
+      serializer.new(presenter, h: helper, auth: auth)
+    end
+
+    def build_rel(opts = {})
+      builder.build opts
+    end
+
+    def relation
+      @relation ||= build_rel
+    end
+
+    def input_schema
+      action.schema
+    end
+
+    def output_schema
+      serializer.json_schema ? serializer.json_schema : {}
+    end
+
+    def authorized?(auth, params)
+      authorizer.authorized?(scope.to_a, auth, params)
+    end
+
+    private
+
+    attr_reader :authorizer
+
+    def query_keys
+      mutating? ? [] : action.schema.structure.keys
+    end
+
+    def mutating?
+      MUTATING_VERBS.include? verb
+    end
+  end
+end
---
+++ spec/endpoint_spec.rb
@@ -0,0 +1,53 @@
+require "spec_helper"
+require "bridger/endpoint"
+
+RSpec.describe Bridger::Endpoint do
+  let(:authorizer) { double('Authorizer') }
+  let(:action) { double('Action') }
+  let(:serializer) { double('Serializer') }
+
+  it "has readers" do
+    endpoint = described_class.new(
+      name: 'create_product',
+      verb: :post,
+      path: '/v1/products',
+      title: 'Create products',
+      scope: 'a.b.c',
+      authorizer: authorizer,
+      action: action,
+      serializer: serializer
+    )
+
+    expect(endpoint.path).to eq '/v1/products'
+    expect(endpoint.verb).to eq :post
+    expect(endpoint.name).to eq "create_product"
+    expect(endpoint.scope).to eq 'a.b.c'
+    expect(endpoint.action).to eq action
+    expect(endpoint.serializer).to eq serializer
+  end
+
+  it "runs action" do
+    endpoint = described_class.new(
+      name: 'create_product',
+      verb: :post,
+      path: '/v1/products',
+      title: 'Create products',
+      scope: 'a.b.c',
+      authorizer: authorizer,
+      action: action,
+      serializer: serializer
+    )
+
+    auth = double('Auth')
+    params = {foo: 'bar'}
+    helper = double('Helper', params: params)
+    presenter = double('Presenter')
+
+    expect(auth).to receive(:authorize!).with('a.b.c', authorizer, params).and_return true
+    expect(action).to receive(:run!).with(payload: {p1: 1}, auth: auth).and_return presenter
+    expect(serializer).to receive(:new).with(presenter, h: helper, auth: auth).and_return({out: 1})
+
+    data = endpoint.run!(payload: {p1: 1}, auth: auth, helper: helper)
+    expect(data).to eq({out: 1})
+  end
+end
