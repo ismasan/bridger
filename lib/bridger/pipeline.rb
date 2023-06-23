0fc00ca1219973e156ab81186d7644a64c28d0f1
Ismael Celis <ismaelct@gmail.com>
2023-06-23 16:22:07 +0100

    Bridger::Pipeline

---
+++ lib/bridger/pipeline.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+module Bridger
+  class Pipeline
+    NOOP = -> (result) { result }
+
+    # The Bind function.
+    # Only forward result from left to right callable if result is not halted.
+    class Bind
+      # @param left [Step]
+      # @param right [Step]
+      def initialize(left, right)
+        @left = left
+        @right = right
+      end
+
+      # @param result [Result]
+      # @return [Result]
+      def call(result)
+        result = @left.call(result)
+        result.halted? ? result : @right.call(result)
+      end
+    end
+
+    # The BindAny function.
+    # Forward result from left to right callable regardless of result being halted.
+    class BindAny
+      # @param left [Step]
+      # @param right [Step]
+      def initialize(left, right)
+        @left = left
+        @right = right
+      end
+
+      # @param result [Result]
+      # @return [Result]
+      def call(result)
+        @right.call(@left.call(result))
+      end
+    end
+
+    def initialize(&config)
+      @pipe = NOOP
+
+      configure(&config) if block_given?
+      freeze
+    end
+
+    def step(callable = nil, &block)
+      callable ||= block
+      raise ArgumentError, "#step expects an interface #call(Result) Result, but got #{callable.inspect}" unless is_a_step?(callable)
+
+      @pipe = Bind.new(@pipe, callable)
+      self
+    end
+
+    def step!(callable = nil, &block)
+      callable ||= block
+      raise ArgumentError, "#step expects an interface #call(Result) Result, but got #{callable.inspect}" unless is_a_step?(callable)
+
+      @pipe = BindAny.new(@pipe, callable)
+      self
+    end
+
+    def halt(&block)
+      step do |result|
+        result.halt(&block)
+      end
+    end
+
+    def continue(&block)
+      step do |result|
+        result.continue(&block)
+      end
+    end
+
+    def call(result)
+      @pipe.call(result)
+    end
+
+    private
+
+    def configure(&setup)
+      case setup.arity
+      when 1
+        setup.call(self)
+      when 0
+        instance_eval(&setup)
+      else
+        raise ArgumentError, 'setup block must have arity of 0 or 1'
+      end
+    end
+
+    def is_a_step?(callable)
+      return false unless callable.respond_to?(:call)
+
+      arity = callable.respond_to?(:arity) ? callable.arity : callable.method(:call).arity
+      arity == 1
+    end
+  end
+end
---
+++ spec/pipeline_spec.rb
@@ -0,0 +1,145 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'bridger/pipeline'
+
+module PipelineTest
+  module With
+    def with(**kwargs)
+      self.class.new(to_h.merge(kwargs))
+    end
+  end
+
+  Result = Struct.new(:name, keyword_init: true) do
+    include With
+
+    def halted?
+      false
+    end
+
+    def halt(&block)
+      result = self
+      if block_given?
+        result = dup
+        yield result
+      end
+      Halt.new(name: result.name)
+    end
+
+    def continue(&block)
+      return self unless block_given?
+
+      result = self.dup
+      yield result
+      result
+    end
+  end
+
+  Halt = Struct.new(:name, keyword_init: true) do
+    include With
+
+    def halted?
+      true
+    end
+
+    def continue(&block)
+      result = self.dup
+      yield result if block_given?
+      Result.new(name: result.name)
+    end
+  end
+end
+
+RSpec.describe Bridger::Pipeline do
+  specify 'basic pipeline' do
+    pipe = Bridger::Pipeline.new do |pl|
+      pl.step do |result|
+        result.with(name: 'John')
+      end
+      pl.step do |result|
+        PipelineTest::Halt.new(name: result.name)
+      end
+      pl.step do |result|
+        result.with(name: 'Joe')
+      end
+    end
+
+    result = pipe.call(PipelineTest::Result.new(name: ''))
+    expect(result.name).to eq('John')
+    expect(result.halted?).to be(true)
+  end
+
+  specify '#halt helper' do
+    pipe = Bridger::Pipeline.new do |pl|
+      pl.step do |result|
+        result.with(name: 'John')
+      end
+      pl.halt do |result|
+        result.name = 'Halted'
+      end
+      pl.step do |result|
+        result.with(name: 'Joe')
+      end
+    end
+
+    result = pipe.call(PipelineTest::Result.new(name: ''))
+    expect(result.name).to eq('Halted')
+    expect(result.halted?).to be(true)
+  end
+
+  specify '#continue helper' do
+    pipe = Bridger::Pipeline.new do |pl|
+      pl.step do |result|
+        result.with(name: 'John')
+      end
+      pl.continue do |result|
+        result.name = 'Continued'
+      end
+    end
+
+    result = pipe.call(PipelineTest::Result.new(name: ''))
+    expect(result.name).to eq('Continued')
+    expect(result.halted?).to be(false)
+  end
+
+  specify '#step! runs for halts too' do
+    pipe = Bridger::Pipeline.new do |pl|
+      pl.step do |result|
+        result.with(name: 'John')
+      end
+      pl.halt
+      pl.step do |r|
+        r.with(name: 'Joe') # will be skipped
+      end
+      pl.step! do |r| # will run even if result is halted
+        r.continue do |r|
+          r.name = 'Jane'
+        end
+      end
+      pl.step do |r|
+        r.with(name: r.name + ' Doe')
+      end
+    end
+
+    result = pipe.call(PipelineTest::Result.new(name: ''))
+    expect(result.name).to eq('Jane Doe')
+    expect(result.halted?).to be(false)
+  end
+
+  specify 'sub pipelines' do
+    john = ->(result) { result.with(name: 'John') }
+    doe = Bridger::Pipeline.new do |pl|
+      pl.step do |r|
+        r.with(name: r.name + ' Doe')
+      end
+    end
+
+    pipe = Bridger::Pipeline.new do |pl|
+      pl.step john
+      pl.step doe
+    end
+
+    result = pipe.call(PipelineTest::Result.new(name: ''))
+    expect(result.name).to eq('John Doe')
+  end
+end
