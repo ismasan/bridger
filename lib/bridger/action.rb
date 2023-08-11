3a2a4f8be5575b9dd903b97f37cc5f7c41c10e2c
Ismael Celis <ismaelct@gmail.com>
2023-08-11 15:22:05 +0100

    Document Bridger::Action

--- lib/bridger/action.rb
+++ lib/bridger/action.rb
@@ -3,6 +3,59 @@
 require "parametric/dsl"
 
 module Bridger
+  # Action is a utility class to encapsulate a Pipeline while
+  # still providing a class that can hold internal state.
+  # Action classes can define #query_schema and #payload_schema
+  # which will be validated as a first step before invoking #run(result).
+  # It is meant to be subclassed.
+  #
+  # @example
+  #
+  #  class MyAction < Bridger::Action
+  #    payload_schema do
+  #      field(:age).type(:integer).default(40)
+  #      field(:name).type(:string).present
+  #    end
+  #
+  #    def run(result)
+  #      # We have `#payload, #query, #auth` available here
+  #      # which are the same as `result.payload, result.query, result.auth`
+  #      result.continue("#{payload[:name]} is #{payload[:age]} years old")
+  #    end
+  #  end
+  #
+  # Action subclasses can override #pipeline to define a custom pipeline.
+  #
+  # @example
+  #
+  # class MyAction < Bridger::Action
+  #   def pipeline
+  #     Bridger::Pipeline.new do |pl|
+  #       pl.step method(:before_schemas)
+  #       pl.step validate_schemas # <- this is built-in
+  #       pl.step method(:after_schemas)
+  #       pl.step! method(:always_run)
+  #     end
+  #   end
+  #
+  #   def before_schemas(result)
+  #     result.continue do |r|
+  #       r[:before] = true
+  #     end
+  #   end
+  #
+  #   def after_schemas(result)
+  #     result.continue do |r|
+  #       r[:after] = true
+  #     end
+  #   end
+  #
+  #   def always_run(result)
+  #     result.copy do |r|
+  #       r[:always] = true
+  #     end
+  #   end
+  # end
   class Action
     include Parametric::DSL
 
@@ -14,26 +67,39 @@ module Bridger
       self.schema *(args.unshift(:query)), &block
     end
 
+    # The [Step] interface is the same as [Pipeline] interface.
+    # @param result [Bridger::Result]
+    # @return [Bridger::Result]
     def self.call(result)
       new(result).run!
     end
 
+    # @param result [Bridger::Result]
     def initialize(result)
       @result = result
-      @validations = Pipeline.new do |pl|
+      @validate_schemas = Pipeline.new do |pl|
         pl.query_schema self.class.query_schema
         pl.payload_schema self.class.payload_schema
+        pl.step { |r| @result = r }
       end
+      @pipeline = build_pipeline
     end
 
+    # @return [Bridger::Result]
     def run!
-      @result = validations.call(result)
-      result.halted? ? result : run(result)
+      pipeline.call(result)
     end
 
     private
 
-    attr_reader :result, :validations
+    attr_reader :result, :validate_schemas, :pipeline
+
+    def build_pipeline
+      Pipeline.new do |pl|
+        pl.step validate_schemas
+        pl.step method(:run)
+      end
+    end
 
     def auth
       @result.auth
--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -37,4 +37,62 @@ RSpec.describe Bridger::Action do
     expect(result.errors['$.name']).not_to be_empty
     expect(result.object).to be_nil
   end
+
+  context 'with custom pipeline' do
+    subject(:action) do
+      Class.new(Bridger::Action) do
+        payload_schema do
+          field(:age).type(:integer).default(40)
+          field(:name).type(:string).present
+        end
+
+        def pipeline
+          Bridger::Pipeline.new do |pl|
+            pl.step method(:before_schemas)
+            pl.step validate_schemas
+            pl.step method(:after_schemas)
+            pl.step! method(:always_run)
+          end
+        end
+
+        def before_schemas(result)
+          result.continue do |r|
+            r[:before] = true
+          end
+        end
+
+        def after_schemas(result)
+          result.continue do |r|
+            r[:after] = true
+          end
+        end
+
+        def always_run(result)
+          result.copy do |r|
+            r[:always] = true
+          end
+        end
+      end
+    end
+
+    specify 'valid payload' do
+      initial = Bridger::Result::Success.build(payload: { name: 'John' })
+      result = action.call(initial)
+      expect(result.halted?).to be(false)
+      expect(result[:before]).to be(true)
+      expect(result[:after]).to be(true)
+      expect(result[:always]).to be(true)
+      expect(result.payload[:age]).to eq(40)
+    end
+
+    specify 'invalid payload' do
+      initial = Bridger::Result::Success.build(payload: { name: '' })
+      result = action.call(initial)
+      expect(result.halted?).to be(true)
+      expect(result[:before]).to be(true)
+      expect(result[:after]).to be_nil
+      expect(result[:always]).to be(true)
+      expect(result.errors['$.name']).not_to be_empty
+    end
+  end
 end
